c65b2303ab7450cb2eda93204ffe2ececcd148cc
JaapGroen <jaap.groen@gmail.com>
2019-12-23 14:55:28 +0000

    set to general

--- src/store/store.js
+++ src/store/store.js
@@ -8,7 +8,7 @@ export default new Vuex.Store({
   state: {
     token: localStorage.getItem('WADtoken') || '',
     user : JSON.parse(localStorage.getItem('WADuser')) || '',
-    api:{ip:'10.10.13.43',port:3000},
+    api:{ip:'<wadapi ip>',port:'<wadapi port>'},
     recipes:''
   },
   mutations: {
